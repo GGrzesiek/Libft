be6813eefa51b67a1d17cd4c1bfd55b016cc5898
Grzegorz Kryszczuk <gkryszcz@c3r3s3.42warsaw.pl>
2024-12-18 13:24:20 +0100

    added 2 functions

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,17 +6,57 @@
 /*   By: gkryszcz <gkryszcz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 15:29:44 by gkryszcz          #+#    #+#             */
-/*   Updated: 2024/12/17 15:56:34 by gkryszcz         ###   ########.fr       */
+/*   Updated: 2024/12/18 13:00:51 by gkryszcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	is_in_set(char c, char const *set)
+{
+	int	j;
+
+	j = 0;
+	while (set[j])
+	{
+		if (c == (char)set[j])
+			return (1);
+		j++;
+	}
+	return (0);
+}
+
 char *ft_strtrim(char const *s1, char const *set)
 {
 	char *newstr;
-	size_t occ;
-
-	if(ft_strchr(s1,))
+	int start;
+	int end;
+	int i;
 	
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && is_in_set(s1[start], set))
+		start++;
+	while (end > 0 && is_in_set(s1[end], set))
+		end--;
+	if (end <= 0)
+		return (ft_calloc(1, sizeof(char)));
+	newstr = (char *)ft_calloc(((end + 1) - start + 1), sizeof(char));
+	if (!newstr)
+		return (NULL);
+	i = -1;
+	while (++i < (end - start + 1))
+		newstr[i] = s1[start + i];
+	newstr[i] = 0;
+	return (newstr);
+}
+
+int	main(void)
+{
+	char *x;
+	x = ft_strtrim(" 121 XD  ", " ");
+	printf("|%s|\n", x);
+	free(x);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gkryszcz <gkryszcz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 17:12:51 by gkryszcz          #+#    #+#             */
-/*   Updated: 2024/12/17 15:29:34 by gkryszcz         ###   ########.fr       */
+/*   Updated: 2024/12/18 10:48:38 by gkryszcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,6 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char 	*ft_strtrim(char const *s1, char const *set);
-
+char 	**ft_split(char const *s, char c);
 
 #endif
